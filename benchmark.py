2a847a586aa71bd47681e86fc0bdce53bf8cd608
MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
2023-11-28 04:27:00 +0200

    Implemented databases creation and data insertion logic

--- benchmark.py
+++ benchmark.py
@@ -10,6 +10,7 @@ import sparkSubmitExecutor
 root_path = None
 logs_path = None
 tmp_path = None
+spark_config_file = 'resources\metrics.properties'
 
 def create_curr_run_folders():
     root_path = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -36,16 +37,36 @@ if __name__ == '__main__':
     data_generator = DataGeneration(tpch_scale_factor)
     data_generator.generate_data(tpch_gen_path)
 
+    #benchmarking
+    spark_connection = config_loader.get_spark_connection()
+    hdfs_connection = config_loader.get_hdfs_connection()
+    hive_connection = config_loader.get_hive_connection()
+    iceberg_warehouse = config_loader.get_iceberg_warehouse()
+    yarn_connection = config_loader.get_yarn_connection()
+    spark_submit_executor = sparkSubmitExecutor(yarn_connection['ip'], yarn_connection['port'], spark_config_file)
+
     hive_temp_manipulator = HiveManipulator()
     iceberg_temp_manipulator = IcebergManipulator()
+
     for i in range(config_loader.get_groups_size()):
         hive_props = config_loader.get_table_properties(i, 'hive')
         iceberg_props = config_loader.get_table_properties(i, 'iceberg')
         #create dummy database (hive and iceberg)
         database_name = f'benchmarking_{str(uuid.uuid4())}'
+        hive_connection_args = [hive_connection['ip'], hive_connection['port'], database_name]
+        iceberg_connection_args = [hdfs_connection['ip'], hdfs_connection['port'], database_name, iceberg_warehouse]
+
         hive_db_temp_path = hive_temp_manipulator.create_database_template(tmp_path)
         iceberg_db_temp_path = iceberg_temp_manipulator.create_database_template(tmp_path)
+        sparkSubmitExecutor.submit_pyspark(hive_db_temp_path, hive_connection_args)
+        sparkSubmitExecutor.submit_pyspark(iceberg_db_temp_path, iceberg_connection_args)
+
         #insert data into tables (hive and iceberg)
+        hive_insertion_temp_path = hive_temp_manipulator.set_creation_template_properties(config_loader.get_table_properties(i, 'hive'), tpch_gen_path, tmp_path)
+        iceberg_insertion_temp_path = iceberg_temp_manipulator.set_creation_template_properties(config_loader.get_table_properties(i, 'iceberg'), tpch_gen_path, tmp_path)
+        sparkSubmitExecutor.submit_pyspark(hive_insertion_temp_path, hive_connection_args)
+        sparkSubmitExecutor.submit_pyspark(iceberg_insertion_temp_path, iceberg_connection_args)
+        
         #loop and run queries
         #collect metrics
         #plot metrics
