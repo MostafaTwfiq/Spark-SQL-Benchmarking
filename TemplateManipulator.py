38eac6d1c976788bc943de7c6d4739de1f348bd8
Radwa Ahmed <radwaahmed20112000@gmail.com>
2023-11-28 14:21:57 +0200

    Resolve: minor issues

--- TemplateManipulator.py
+++ TemplateManipulator.py
@@ -5,44 +5,52 @@ import uuid
 
 class TemplateManipulator:
 
-    def __init__(self):
+    def __init__(self, output_folder):
+        self.TEMPLATES_FOLDER = './Templates'
         self.QUERY_PLACEHOLDER = 'QUERY'
+        self.output_folder = output_folder
         
-    def set_creation_template_properties(self):
+    def set_creation_template_properties(self, table_properties, generated_tables_folder):
         pass
 
     def set_query(self, query):
         pass
 
-    def replace_words_in_file(self, file_path, modifications):
+    def replace_words_in_file(self, input_file, output_file, modifications):
         # Read the content of the file
-        with open(file_path, 'r') as file:
+        with open(input_file, 'r') as file:
             template_content = file.read()
 
         template = Template(template_content)
         modified_content = template.render(modifications)
 
-        unique_id = uuid.uuid4()
-        base_name, extension = os.path.splitext(file_path)
-        new_file_path = f'{base_name}_{unique_id}{extension}'
-
         # Write the modified content back to the file
-        with open(os.path.join('./temp', new_file_path), 'w') as file:
+        with open(output_file, 'w') as file:
             file.write(modified_content)
         
 
 class HiveManipulator(TemplateManipulator):
 
-    def __init__(self):
-        super().__init__()
-        self.PARTITION_PLACEHOLDER = 'partitioning_dict'
+    def __init__(self, output_folder):
+        super().__init__(output_folder)
+        self.PARTITION_PLACEHOLDER = 'PARTITIONING_DICT'
+        self.GENERATE_TABLES_FOLDER = 'GENERATED_TABLES_FOLDER'
 
 
-    def set_creation_template_properties(self, table_properties):
-        partitioning_dict = self.__extract_or_default_properties(table_properties) 
-
-        self.replace_words_in_file('DataCreationHive.py', 
-                                   {self.PARTITION_PLACEHOLDER: partitioning_dict})
+    def set_creation_template_properties(self, table_properties, generated_tables_folder):
+        partitioning_dict = self.__extract_or_default_properties(table_properties)
+        print("HELLOOOO " + generated_tables_folder)
+        
+        input_file_path = os.path.join(self.TEMPLATES_FOLDER, 'DataCreationHive.py')
+        output_file_path = os.path.join(self.output_folder, f'DataCreationHive_{str(uuid.uuid4())}.py')
+        try:
+            self.replace_words_in_file(input_file_path, output_file_path, {self.PARTITION_PLACEHOLDER: partitioning_dict,
+                                    self.GENERATE_TABLES_FOLDER: generated_tables_folder})
+            return output_file_path
+        except Exception as e:
+            # TODO: Log the exception here
+            print(f"An error occurred: {e}")
+            return None
         
 
     def __extract_or_default_properties(self, table_properties):
@@ -54,30 +62,62 @@ class HiveManipulator(TemplateManipulator):
         return partitioning_dict
     
     def set_query(self, query):
-        self.replace_words_in_file('QueryHive.py', {self.QUERY_PLACEHOLDER: query})
+        input_file_path = os.path.join(self.TEMPLATES_FOLDER, 'QueryHive.py')
+        output_file_path = os.path.join(self.output_folder, f'QueryHive_{str(uuid.uuid4())}.py')
+
+        try:
+            self.replace_words_in_file(input_file_path, output_file_path, {self.QUERY_PLACEHOLDER: query})
+            return output_file_path
+        except Exception as e:
+            # TODO: Log the exception here
+            print(f"An error occurred: {e}")
+            return None
+        
+    def create_database_template(self):
+        input_file_path = os.path.join(self.TEMPLATES_FOLDER, 'HiveDataBaseCreation.py')
+        output_file_path = os.path.join(self.output_folder, f'HiveDataBaseCreation_{str(uuid.uuid4())}.py')
+
+        try:
+            self.replace_words_in_file(input_file_path, output_file_path, {})
+            return output_file_path
+        except Exception as e:
+            # TODO: Log the exception here
+            print(f"An error occurred: {e}")
+            return None
+
 
 
 
 class IcebergManipulator(TemplateManipulator):
 
-    def __init__(self):
-        super().__init__()
-        self.PARTITION_PLACEHOLDER = 'partitioning_dict'
+    def __init__(self, output_folder):
+        super().__init__(output_folder)
+        self.PARTITION_PLACEHOLDER = 'PARTITIONING_DICT'
         self.DELETE_PLACEHOLDER = 'DELETE_MODE'
         self.UPDATE_PLACEHOLDER = 'UPDATE_MODE'
         self.MERGE_PLACEHOLDER = 'MERGE_MODE'
+        self.GENERATE_TABLES_FOLDER = 'GENERATED_TABLES_FOLDER'
 
 
-    def set_creation_template_properties(self, table_properties):
+    def set_creation_template_properties(self, table_properties, generated_tables_folder):
         partitioning_dict, delete_mode, update_mode, merge_mode \
             = self.__extract_or_default_properties(table_properties) 
         
         modifications = {self.PARTITION_PLACEHOLDER: partitioning_dict, 
                          self.DELETE_PLACEHOLDER: delete_mode,
                          self.UPDATE_PLACEHOLDER: update_mode,
-                         self.MERGE_PLACEHOLDER: merge_mode}
+                         self.MERGE_PLACEHOLDER: merge_mode,
+                         self.GENERATE_TABLES_FOLDER: generated_tables_folder}
         
-        self.replace_words_in_file('DataCreationIceberg.py', modifications)
+        input_file_path = os.path.join(self.TEMPLATES_FOLDER, 'DataCreationIceberg.py')
+        output_file_path = os.path.join(self.output_folder, f'DataCreationIceberg_{str(uuid.uuid4())}.py')
+        try:
+            self.replace_words_in_file(input_file_path, output_file_path, modifications)
+            return output_file_path
+        except Exception as e:
+            # TODO: Log the exception here
+            print(f"An error occurred: {e}")
+            return None
 
 
     def __extract_or_default_properties(self, table_properties):
@@ -99,7 +139,28 @@ class IcebergManipulator(TemplateManipulator):
         return partitioning_dict, delete_mode, update_mode, merge_mode
 
     def set_query(self, query):
-        self.replace_words_in_file('QueryIceberg.py', {self.QUERY_PLACEHOLDER: query})        
+        input_file_path = os.path.join(self.TEMPLATES_FOLDER, 'QueryIceberg.py')
+        output_file_path = os.path.join(self.output_folder, f'QueryIceberg_{str(uuid.uuid4())}.py')
+
+        try:
+            self.replace_words_in_file(input_file_path, output_file_path, {self.QUERY_PLACEHOLDER: query})
+            return output_file_path
+        except Exception as e:
+            # TODO: Log the exception here
+            print(f"An error occurred: {e}")
+            return None  
+
+    def create_database_template(self):
+        input_file_path = os.path.join(self.TEMPLATES_FOLDER, 'IcebergDataBaseCreation.py')
+        output_file_path = os.path.join(self.output_folder, f'IcebergDataBaseCreation_{str(uuid.uuid4())}.py')
+
+        try:
+            self.replace_words_in_file(input_file_path, output_file_path, {})
+            return output_file_path
+        except Exception as e:
+            # TODO: Log the exception here
+            print(f"An error occurred: {e}")
+            return None    
     
 
 # from ConfigurationLoader import ConfigurationLoader;
@@ -111,11 +172,11 @@ class IcebergManipulator(TemplateManipulator):
 #     len = conf_loader.get_groups_size()
 #     for i in range(len):
 #         hive_props = conf_loader.get_table_properties(i, 'hive')
-#         hive_loader = HiveManipulator()
-#         hive_loader.set_creation_template_properties(hive_props)
+#         hive_loader = HiveManipulator('./temp')
+#         hive_loader.set_creation_template_properties(hive_props, 'HI')
 #         hive_loader.set_query("select * from nation;")
 
 #         iceberg_props = conf_loader.get_table_properties(i, 'iceberg')
-#         iceberg_loader = IcebergManipulator()
-#         iceberg_loader.set_creation_template_properties(iceberg_props)
+#         iceberg_loader = IcebergManipulator('./temp')
+#         iceberg_loader.set_creation_template_properties(iceberg_props, 'HI')
 #         iceberg_loader.set_query("select * from nation;")
