272656eae581059ea6cad7f97491dd58e0f32d57
Moustaf Mohamed ibrahim Twfiq Ali <mmiali@ejadaalex.local>
2023-11-30 01:03:54 +0200

    Fixed some bugs and added console logging.

--- benchmark.py
+++ benchmark.py
@@ -9,13 +9,12 @@ from SparkRestAPI import SparkRestAPI
 from MetricsPlotter import MetricsPlotter
 from SparkSubmitExecutor import SparkSubmitExecutor
 import re 
-from Utils import get_random_string
 
 root_path = None
 logs_path = None
 tmp_path = None
 metric_path = None
-spark_config_file = 'resources\metrics.properties'
+spark_config_file = './resources/metrics.properties'
 
 def create_curr_run_folders():
     global root_path, logs_path, tmp_path, metric_path
@@ -52,10 +51,12 @@ def read_benchmark_queries(sql_file_path):
 if __name__ == '__main__':
     # Create new folder for the current app run
     create_curr_run_folders()
+    print("Benchmark folders created successfully.") # Logging
 
     # Load configuration file
     config_file_path = sys.argv[1]
     config_loader = ConfigurationLoader(config_file_path)
+    print("Configuration file loaded successfully.") # Logging
 
     # Generate tpch data
     tpch_dbgen_path = sys.argv[2]
@@ -76,50 +77,70 @@ if __name__ == '__main__':
     iceberg_temp_manipulator = IcebergManipulator(tmp_path)
 
     for i in range(config_loader.get_groups_size()):
+        print(f"Benchmarking properties group {i}:") #Logging
+        
+        # Loading current properties group
         hive_props = config_loader.get_table_properties(i, 'hive')
         iceberg_props = config_loader.get_table_properties(i, 'iceberg')
+
         # Create dummy database (hive and iceberg)
-        hive_database_name = f'benchmarking_{get_random_string(3)}'        
-        iceberg_database_name = f'benchmarking_{get_random_string(3)}'
-        print(hive_database_name)
-        print(iceberg_database_name)
+        hive_database_name = f'benchmarking_{str(uuid.uuid4())}'   
+        iceberg_database_name = f'benchmarking_{str(uuid.uuid4())}'
         
         hive_connection_args = [hive_connection['ip'], str(hive_connection['port']), hive_database_name]
         iceberg_connection_args = [hdfs_connection['ip'], str(hdfs_connection['port']), iceberg_database_name, iceberg_warehouse]
 
         hive_db_temp_path = hive_temp_manipulator.create_database_template()
+        print(f"\tDone creating hive database template: {hive_db_temp_path}") # Logging
         iceberg_db_temp_path = iceberg_temp_manipulator.create_database_template()
+        print(f"\tDone creating iceberg database template: {iceberg_db_temp_path}") # Logging
         spark_submit_executor.submit_pyspark(hive_db_temp_path, hive_connection_args)
+        print(f"\tDone creating hive database: {hive_database_name}") # Logging
         spark_submit_executor.submit_pyspark(iceberg_db_temp_path, iceberg_connection_args)
+        print(f"\tDone creating iceberg database: {iceberg_database_name}") # Logging
 
         #insert data into tables (hive and iceberg)
         hive_insertion_temp_path = hive_temp_manipulator. \
             set_creation_template_properties(config_loader.get_table_properties(i, 'hive'), tpch_gen_path)
+        print(f"\tDone creating hive records insertion template: {hive_insertion_temp_path}") # Logging
         iceberg_insertion_temp_path = iceberg_temp_manipulator. \
             set_creation_template_properties(config_loader.get_table_properties(i, 'iceberg'), tpch_gen_path)
+        print(f"\tDone creating iceberg records insertion template: {iceberg_insertion_temp_path}") # Logging
         spark_submit_executor.submit_pyspark(hive_insertion_temp_path, hive_connection_args)
+        print(f"\tRecords successfully inserted in hive tables.") # Logging
         spark_submit_executor.submit_pyspark(iceberg_insertion_temp_path, iceberg_connection_args)
+        print(f"\tRecords successfully inserted in iceberg tables.") # Logging
         
         # Loop and run queries
         hive_durations = []
         iceberg_durations = []
         rest = SparkRestAPI(spark_connection['ip'], spark_connection['port'])
         queries_names, dql_benchmark_queries = read_benchmark_queries('benchmark_queries.sql')
-        for query in dql_benchmark_queries:
+        print("\tBenchmark queries loaded successfully.")
+        for query_name, query in zip(queries_names, dql_benchmark_queries):
+            print(f"\t Benchmarking {query_name} query:")
+
             # Creating Output Scripts
             hive_query_temp_path = hive_temp_manipulator.set_query(query)
+            print(f"\t\tDone creating hive SQL execution template: {hive_query_temp_path}") # Logging
             iceberg_query_temp_path = iceberg_temp_manipulator.set_query(query)
+            print(f"\t\tDone creating iceberg SQL execution template: {iceberg_query_temp_path}") # Logging
             
             # Submit Output Scripts to spark-submit
             hive_app_id = spark_submit_executor.submit_pyspark(hive_query_temp_path, hive_connection_args)
+            print(f"\t\tCompleted running the query on hive.") # Logging
             iceberg_app_id = spark_submit_executor.submit_pyspark(iceberg_query_temp_path, iceberg_connection_args)
+            print(f"\t\tCompleted running the query on iceberg.") # Logging
         
             # Fetch Query Duration
             hive_durations.append(rest.get_sql_duration(rest.get_application_all_sql_metrics(hive_app_id)))
+            print("\t\tQuery duration on hive fetched successfully.") # Logging
             iceberg_durations.append(rest.get_sql_duration(rest.get_application_all_sql_metrics(iceberg_app_id)))
+            print("\t\tQuery duration on iceberg fetched successfully.") # Logging
                     
         # Plot metrics
         plotter = MetricsPlotter()
         plotter.plot_benchmark_results(queries=queries_names, 
                                    tables_metrics={'hive': hive_durations, 'iceberg': iceberg_durations},
                                    metric_type='duration', title='Hive vs Iceberg')
+        print("\tDone plotting metrics.")
